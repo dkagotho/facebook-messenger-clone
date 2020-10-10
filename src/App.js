fb6d83fd924f0a30ff459aa9a200c8f806ce2793
Diana kagotho <dianakago2@gmail.com>
2020-10-10 14:07:36 -0400

    introduced user names into the messages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
 
 import "./App.css";
+import Message from "./Message";
 
 function App() {
-  const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([{username: 'Diana', text:'Hey yoh'},{username: 'Delany', text:'babe'}]);
+  const [username, setUsername] = useState('');
 
-  console.log(input);
-  console.log(messages);
+  useEffect(() => {
+    setUsername(prompt('Please enter your name'))
+  }, [])
 
   const sendMessage = (event) => {
     //to prevent the refresh from happening on the whole page when you hit enter
     event.preventDefault();
     //all the logic to send the message goes here
-    setMessages([...messages, input]);
+    setMessages([...messages, {username: username, text: input}]);
     setInput("");
   };
 
   return (
     <div className="App">
       <h1>Hello Diana</h1>
-
+      <h2>welcome {username}</h2>
     <form>
       <FormControl>
         <InputLabel>Enter a message...</InputLabel>
@@ -31,9 +34,10 @@ function App() {
           </Button>
       </FormControl>
     </form>
-    
-      {messages.map((message) => (
-        <p>{message}</p>
+
+      {
+        messages.map((message) => (
+          <Message username={message.username} text={message.text} />
       ))
       }
     </div>
